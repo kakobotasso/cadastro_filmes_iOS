6d24fa1c2b6d4c0835696397cb7c1db04f8c20f2
Kako Botasso <kakodev@gmail.com>
2017-03-17 22:00:35 -0300

    CRUD iOS

--- MoviesLib/MoviesTableViewController.swift
+++ MoviesLib/MoviesTableViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class MoviesTableViewController: UITableViewController {
+    
+    var fetchedResultController: NSFetchedResultsController<Movie>!
+    var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +26,12 @@ class MoviesTableViewController: UITableViewController {
         tableView.estimatedRowHeight = 106
         tableView.rowHeight = UITableViewAutomaticDimension
         
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 22))
+        label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 22))
         label.text = "Sem filmes"
         label.textAlignment = .center
         label.textColor = .white
         
-        tableView.backgroundView = label
+        loadMovies()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,10 +40,30 @@ class MoviesTableViewController: UITableViewController {
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // SÓ ENTRA NO IF SE ELE ESTIVER INDO PARA O DETALHE DO FILME
+        if let vc = segue.destination as? MovieViewController {
+            vc.movie = fetchedResultController.object(at: tableView.indexPathForSelectedRow!)
+        }
     }
     
     // MAKK: - Methods
-    
+    func loadMovies(){
+        let fetchRequest: NSFetchRequest<Movie> = Movie.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        fetchedResultController.delegate = self
+        
+        do{
+            try fetchedResultController.performFetch()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        
+    }
     
     // MARK: - Table view data source
 
@@ -49,42 +73,50 @@ class MoviesTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0 //dataSource.count
+        if let count = fetchedResultController.fetchedObjects?.count {
+            tableView.backgroundView = (count == 0) ? label : nil
+            return count
+        } else {
+            tableView.backgroundView = label
+            return 0
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as! MovieTableViewCell
-
-        /*
-        cell.ivPoster.image = UIImage(named: dataSource[indexPath.row].imageSmall)
-        cell.lbTitle.text = dataSource[indexPath.row].title
-        cell.lbRating.text = "\(dataSource[indexPath.row].rating)"
-        cell.lbSummary.text = dataSource[indexPath.row].summary
-        */
         
+        let movie = fetchedResultController.object(at: indexPath)
+        
+        cell.lbTitle.text = movie.title
+        cell.lbRating.text = "\(movie.rating)"
+        cell.lbSummary.text = movie.summary
+
         return cell
     }
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+ 
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let movie = fetchedResultController.object(at: indexPath)
+            context.delete(movie)
+            do{
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
@@ -111,4 +143,10 @@ class MoviesTableViewController: UITableViewController {
     }
     */
 
+}
+
+extension MoviesTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.reloadData()
+    }
 }
