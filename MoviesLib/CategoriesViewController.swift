d4ba655a303a3483894ddde43626b6cbaa6c879f
Kako Botasso <kakodev@gmail.com>
2017-03-17 22:50:54 -0300

    Cria cadastro de categorias

---
+++ MoviesLib/CategoriesViewController.swift
@@ -0,0 +1,78 @@
+//
+//  CategoriesViewController.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 17/03/17.
+//  Copyright © 2017 EricBrito. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoriesViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var dataSource: [Category] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+    
+    func loadCategories(){
+        let fetchRequest: NSFetchRequest<Category> = Category.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            dataSource = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func close(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func add(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Adicionar categoria", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { (textField: UITextField) in
+            textField.placeholder = "Nome da categoria"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Adiciona", style: .default, handler: { (action: UIAlertAction) in
+            let category = Category(context: self.context)
+            category.name = alert.textFields?.first?.text
+            
+            do {
+                try self.context.save()
+                self.tableView.reloadData()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+
+}
+
+extension CategoriesViewController: UITableViewDelegate {
+}
+
+extension CategoriesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = self.dataSource[indexPath.row].name
+        return cell!
+    }
+}
